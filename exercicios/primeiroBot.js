3e02194c63d8def376268a0f0e460d3b33489a0d
rodrigomarquespessoa <44514371+rodrigomarquespessoa@users.noreply.github.com>
2018-12-09 00:45:37 -0200

    teste

--- exercicios/primeiroBot.js
+++ exercicios/primeiroBot.js
@@ -3,12 +3,13 @@ console.log(env.token)
 const Telegraf = require('telegraf')
 
 const API_TOKEN = env.token;
+console.log('tk:' ||API_TOKEN)
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://rmpcursobot.herokuapp.com/';
 
-const bot = new Telegraf(env.token)
+const bot = new Telegraf(API_TOKEN)
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 
 bot.start(ctx => {
     const from = ctx.update.message.from
@@ -61,3 +62,11 @@ bot.on('location',(ctx,next) => {
 
 
 bot.startPolling()
+
+// and at the end just start server on PORT
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
